e1e8e54c65fec086141a1967743928f432f68e28
Emilee Chen <emilee.ty.chen@gmail.com>
2016-04-14 21:41:25 -0700

    Add modal for lookbook

--- script/modal.js
+++ script/modal.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-	$(".block").click(function() {
+	$(".img-block").click(function() {
 		$(".lightbox-hidden").addClass("lightbox");
+		var src = $(this).attr("src");
+		$(".modal-img").attr("src", src);
+		// scrollLock();
 	});
 	$("#modal").click(function() {
 		$("#modal").removeClass("lightbox");
+		// scrollLock();
 	});
 });
+
+
+// var locked = false;
+
+// function scrollLock() {
+// 	if (locked) {
+// 		$('body').css( 'overflow', 'visible' );
+// 		locked = false;
+// 	}
+// 	else {
+// 		$('body').css( 'overflow', 'hidden' );
+//   		locked = true;
+// 	}
+// };
